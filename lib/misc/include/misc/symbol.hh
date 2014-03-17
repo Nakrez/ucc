01a94fdddf2ac105606c9c240a092eef80251440
Baptiste Covolato <b.covolato@gmail.com>
2014-03-17 18:48:23 +0100

    [UCC] Introduce libmisc

---
+++ lib/misc/include/misc/symbol.hh
@@ -0,0 +1,15 @@
+#ifndef UCC_MISC_SYMBOL_HH
+# define UCC_MISC_SYMBOL_HH
+
+namespace ucc
+{
+    namespace misc
+    {
+        class Symbol
+        {
+
+        };
+    } // namespace misc
+} // namespace ucc
+
+#endif /* !UCC_MISC_SYMBOL_HH */
