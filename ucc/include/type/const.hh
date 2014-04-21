e9ec49376c0abe4c1224ba8e0e66debac41a466e
Baptiste Covolato <b.covolato@gmail.com>
2014-04-21 11:58:46 +0200

    [UCC][TYPE] Add Const class

---
+++ ucc/include/type/const.hh
@@ -0,0 +1,72 @@
+/*
+Copyright (C) 2014 Baptiste Covolato <b.covolato@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef UCC_TYPE_CONST_HH
+# define UCC_TYPE_CONST_HH
+
+# include <type/type.hh>
+
+namespace ucc
+{
+    namespace type
+    {
+        /// Implements the const qualifier with a type to ease const checking
+        /// in the TypeChecker.
+        class Const : public Type
+        {
+            public:
+                Const(Type* inner_type);
+                virtual ~Const();
+
+                /// \brief  Check compatibility of Const type on assignement
+                /// \param  t   The type to check compatibility with
+                /// \param  op  The assignment operator
+                /// \return Type compatibility level
+                virtual TypeCompatibility
+                compatible_on_assign(const Type& t,
+                                     ast::AssignExpr::AssignOp op) const override;
+
+                /// \brief  Check compatibility of Const type on operation
+                /// \param  t   The type to check compatibility with
+                /// \param  op  The binary operator
+                /// \return Type compatibility level
+                virtual TypeCompatibility
+                compatible_on_op(const Type& t,
+                                 ast::OpExpr::Op op) const override;
+
+                /// Return the inner type because Const type is bypassed when
+                /// checking types, it is only used by TypeChecker on
+                /// assignation.
+                virtual const Type& actual_type() const override
+                {
+                    return *inner_type_;
+                }
+
+                /// Return a string representing the Const type.
+                virtual std::string to_str() const override
+                {
+                    return inner_type_->to_str() + "const";
+                }
+
+            private:
+                Type* inner_type_;
+        };
+    } // namespace type
+} // namespace ucc
+
+#endif /* !UCC_TYPE_CONST_HH */
---
+++ ucc/src/type/const.cc
@@ -0,0 +1,46 @@
+/*
+Copyright (C) 2014 Baptiste Covolato <b.covolato@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <type/const.hh>
+
+using namespace ucc;
+using namespace type;
+
+Const::Const(Type* inner_type)
+    : inner_type_(inner_type)
+{}
+
+Const::~Const()
+{
+    // Inner type is deleted by the TypeBuilder no or it's a
+    // Singleton (if it is a builtin type)
+}
+
+Type::TypeCompatibility
+Const::compatible_on_assign(const Type& t,
+                            ast::AssignExpr::AssignOp op) const
+{
+    return inner_type_->compatible_on_assign(t, op);
+}
+
+Type::TypeCompatibility
+Const::compatible_on_op(const Type& t,
+                        ast::OpExpr::Op op) const
+{
+    return inner_type_->compatible_on_op(t, op);
+}
