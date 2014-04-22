f91f1046de518a68121118f9778892c324b5aeda
Baptiste Covolato <b.covolato@gmail.com>
2014-04-22 11:37:50 +0200

    [UCC][TYPE] Add Record and Field class

---
+++ ucc/include/type/field.hh
@@ -0,0 +1,63 @@
+/*
+Copyright (C) 2014 Baptiste Covolato <b.covolato@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef UCC_TYPE_FIELD_HH
+# define UCC_TYPE_FIELD_HH
+
+# include <misc/symbol.hh>
+
+# include <type/type.hh>
+
+namespace ucc
+{
+    namespace type
+    {
+        class Field
+        {
+            public:
+                /// \brief  Constructor
+                /// \param  name    The name of the field
+                /// \param  type    The type of the field
+                Field(const ucc::misc::Symbol name, Type* type)
+                    : name_(name)
+                    , type_(type)
+                {}
+
+                /// Destructor
+                ~Field() = default;
+
+                /// Return the name of the field
+                const ucc::misc::Symbol& name_get() const
+                {
+                    return name_;
+                }
+
+                /// Return the type of the field
+                const Type* type_get() const
+                {
+                    return type_;
+                }
+
+            private:
+                ucc::misc::Symbol name_;
+                Type* type_;
+        };
+    } // namespace type
+} // namespace ucc
+
+#endif /* !UCC_TYPE_FIELD_HH */
---
+++ ucc/include/type/record.hh
@@ -0,0 +1,90 @@
+/*
+Copyright (C) 2014 Baptiste Covolato <b.covolato@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef UCC_TYPE_RECORD_HH
+# define UCC_TYPE_RECORD_HH
+
+# include <list>
+
+# include <misc/symbol.hh>
+
+# include <type/type.hh>
+# include <type/field.hh>
+
+namespace ucc
+{
+    namespace type
+    {
+        class Record : public Type
+        {
+            public:
+                /// Constructor
+                Record(const ucc::misc::Symbol name, bool is_struct = true)
+                    : name_(name)
+                    , is_struct_(is_struct)
+                {}
+
+                /// Destructor
+                virtual ~Record() = default;
+
+                /// \brief  Add a new Field to the Record.
+                /// \param  f   The field you want to add to the Record.
+                void field_add(const Field& f)
+                {
+                    fields_.push_back(f);
+                }
+
+                /// \brief  Get the Type of a Field named @a name.
+                /// \param  name    The name of the Field you want to get the
+                ///                 Type.
+                /// \return The type of the Field, or @a nullptr if the Field
+                ///         does not exist.
+                const Type* field_type_get(const ucc::misc::Symbol name) const;
+
+                virtual Type::TypeCompatibility
+                compatible_on_assign(const Type&,
+                                     ast::AssignExpr::AssignOp) const override;
+
+                virtual TypeCompatibility
+                compatible_on_op(const Type&, ast::OpExpr::Op) const override
+                {
+                    return Type::TypeCompatibility::error;
+                }
+
+                virtual const Type& actual_type() const override
+                {
+                    return *this;
+                }
+
+                virtual std::string to_str() const override
+                {
+                    if (is_struct_)
+                        return "struct " + name_.data_get();
+                    else
+                        return "union " + name_.data_get();
+                }
+
+            private:
+                ucc::misc::Symbol name_;
+                bool is_struct_;
+                std::list<Field> fields_;
+        };
+    } // namespace type
+} // namespace ucc
+
+#endif /* !UCC_TYPE_RECORD_HH */
