07282c4b8c38eea270690df65a7614fb0076a822
Baptiste Covolato <b.covolato@gmail.com>
2014-06-08 22:32:08 +0200

    [TEST] Add some test for generation of ForStmt and DoWhileStmt

---
+++ tests/ucc/gen/input/do-while-break.c
@@ -0,0 +1,13 @@
+int main(void)
+{
+    int i = 0;
+
+    do
+    {
+        i = i + 1;
+
+        if (i == 10)
+            break;
+
+    } while (i < 30);
+}
---
+++ tests/ucc/gen/input/do-while-continue.c
@@ -0,0 +1,13 @@
+int main(void)
+{
+    int i = 0;
+
+    do
+    {
+        i = i + 1;
+
+        if (i == 10)
+            continue;
+
+    } while (i < 30);
+}
